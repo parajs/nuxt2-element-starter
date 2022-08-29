d086b267c627f12ee3b8e018269397377e0df2d3
chenguzhen87 <546369005@qq.com>
2022-08-29 12:12:23 +0800

    chore(display.css): 添加样式

--- plugins/element-ui.js
+++ plugins/element-ui.js
@@ -1,4 +1,5 @@
 import Element from "element-ui";
+import "element-ui/lib/theme-chalk/display.css";
 // import locale from 'element-ui/lib/locale/lang/en'
 import Vue from "vue";
 // Vue.use(Element);
